c7279ac1a75cbb761c7302bb8b0922fbd53d45fe
Ezequiel Maraschio <ezequiel.maraschio@gmail.com>
2017-11-11 15:10:47 -0300

    API: Add health service

--- noteboard-api/app/controllers/application_controller.rb
+++ noteboard-api/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def health
+    render plain: "OK"
+  end
 end
--- noteboard-api/config/routes.rb
+++ noteboard-api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'health', to: 'application#health'
   namespace :api do
     namespace :v1 do
       resources :notes
