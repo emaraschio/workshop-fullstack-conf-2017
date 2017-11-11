07aad90f6beae1cf3121e6049d4a0d05d9cc940a
Ezequiel Maraschio <ezequiel.maraschio@gmail.com>
2017-11-11 15:10:22 -0300

    API & Client: Change external ports

--- noteboard-api/config/application.rb
+++ noteboard-api/config/application.rb
@@ -23,7 +23,7 @@ module RailsApp
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins ENV["NOTEBOARD_CLIENT_URL"]
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
