5ba743114d4e3427a92da905732f53929f10d601
Ezequiel Maraschio <ezequiel.maraschio@gmail.com>
2017-11-23 08:55:41 -0300

    Terraform: Update ami id and add tags for demo

--- terraform-example/main.tf
+++ terraform-example/main.tf
@@ -1,11 +1,17 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 # Create an EC2 instance
 resource "aws_instance" "example_fullstack_conf" {
-  # AMI ID for Amazon Linux AMI 2017.09.0 (HVM)
-  ami = "ami-c5062ba0"
+  # AMI ID for Amazon Linux AMI 2017.09.1 (HVM), SSD Volume Type
+  ami = "ami-55ef662f"
   instance_type = "t2.micro" # Free Tier
+
+  #tags {
+  #    Name = "terraform-demo-fullstack-conf"
+  #}
 }
+
+
